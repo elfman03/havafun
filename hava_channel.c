26314d76d6b39d279897d5a5d6dce0d0446239a3
elfman09 <elfman09@927543fa-963b-48be-bac8-8f959dc1621d>
2009-08-22 15:35:28 +0000

    LGPL to GPL with exception that tries to cover eventual use with Hava's SDK when it happens.

--- hava_channel.c
+++ hava_channel.c
@@ -1,27 +1,46 @@
 /*
-* Code to change channels or push buttons on the Hava media streamer
-*
-* Copyright (C) 2009 Chris Elford
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * HavaFun code component
+ * Code to change channels or push buttons on the Hava media streamer
+ *
+ * Copyright (C) 2009 Chris Elford
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking havafun statically or dynamically with other modules is making a 
+ * combined work based on havafun. Thus, the terms and conditions of the GNU 
+ * General Public License cover the whole combination.
+ *
+ * In addition, as a special exception, the copyright holders of havafun give 
+ * you permission to combine havafun with free software programs or libraries 
+ * that are released under the GNU LGPL and with code included in the standard 
+ * release of "myhava.com Hava Player platform SDKs (e.g., linux, mac, windows)"
+ * under "myhava.com's SDK license" (or modified versions of such code, with 
+ * unchanged license). You may copy and distribute such a system following the 
+ * terms of the GNU GPL for havafun and the licenses of the other code concerned
+ * {, provided that you include the source code of that other code when and as 
+ * the GNU GPL requires distribution of source code}.
+ *
+ * Note that people who make modified versions of havafun are not obligated to 
+ * grant this special exception for their modified versions; it is their choice
+ * whether to do so. The GNU General Public License gives permission to release
+ * a modified version without this exception; this exception also makes it 
+ * possible to release a modified version which carries forward this exception.
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <assert.h>
 
 //#define DEBUG_MODE 1
@@ -31,13 +50,17 @@
 #endif
 
 #ifdef VSTUDIO
+#define MSLEEP(a) Sleep(a)
 #include <windows.h>
 #include <winbase.h>
 //#include <winsock2.h>
 #else
+#define MSLEEP(a) usleep(a*1000)
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
 #endif
 
 // Initialization packet
@@ -127,7 +150,7 @@ int wait_for_ack() {
       ct=8;
     }
     if(len<=0) {
-      usleep(250000);
+      MSLEEP(250);
     } 
 #ifdef DEBUG_MODE
     else 
